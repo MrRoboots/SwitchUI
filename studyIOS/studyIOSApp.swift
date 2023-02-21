8da2f3bbb0fd051d3f7825594e960d84bd091ae9
MrRoboots <53284445+MrRoboots@users.noreply.github.com>
2023-02-21 14:59:39 +0800

    Initial Commit

---
+++ studyIOS/studyIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  studyIOSApp.swift
+//  studyIOS
+//
+//  Created by YunXing on 2023/2/21.
+//
+
+import SwiftUI
+
+@main
+struct studyIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
